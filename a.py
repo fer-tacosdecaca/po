9c28f1864261afcc0900705515e8dc36494642c1
fer-tacosdecaca <130688038+fer-tacosdecaca@users.noreply.github.com>
2024-02-26 20:01:29 -0600

    a.py

--- a.py
+++ a.py
@@ -1,25 +1,36 @@
 from tkinter import*
 import random
 
-ad = Tk()
-ad.title("Trata de ver esto")
-ad.geometry("400x400")
 
-ñabel = Label(ad)
+
+a = Tk()
+a.geometry("1000x1001")
+a.title("esto no es un cuadrado perfecto")
+
+ñabel = Label(a)
 ñabel.place(relx=0.5, rely=0.5, anchor=CENTER)
+ñabel2 = Label(a)
+ñabel2.place(relx=0.5, rely=0.7, anchor=CENTER)
+
+list = ["uuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuu","bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb","pppppppppppppppppppp                                       ppppppppppppppppppppppppppppppppppppppppppppp"]
+print(list)
+
+
 
 def randomlisto():
-    ran_list = random.sample(range(0,101),1)
-    prize = random.sample(range(0,101),1)
-    print(ran_list)
-    print(prize)
-    if(ran_list == prize):
-        ñabel["text"]="ganaste"
-    
+    random_no = random.randint(0,2)
+    if(random_no == 0):
+        ñabel["text"]="salio 1"
+        g=list[random_no]
+    if(random_no == 1):
+        ñabel["text"]="salio 2"
+        g=list[random_no]
+    if(random_no == 2):
+        g=list[random_no]
+        ñabel["text"]="salio 3"
+        ñabel2["text"]=g
+        
+btn = Button(a, text="ga",command=randomlisto )
+btn.place(relx=0.5, rely=0.7, anchor=CENTER)
 
-    
-btn = Button(ad, text="ga",command=randomlisto )
-btn.place(relx=0.4, rely=0.5, anchor=CENTER)
-
-
-ad.mainloop()
+a.mainloop()
